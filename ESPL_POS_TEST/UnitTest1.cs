c901a0fa2fa6524bb809e7da17fb320364b88243
tbrandon <tbrandon@esplanade.com>
2016-10-26 14:43:59 +0800

    Commit Test

---
+++ ESPL_POS_TEST/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESPL_POS;
+
+namespace ESPL_POS_TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Product p = new Product();
+            
+           
+        }
+    }
+}
