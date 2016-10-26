c8b13e698526f452b57923ed6e2e2a4a58ff2b44
tzeekee <tzeekee@esplanade.com>
2016-10-26 15:01:05 +0800

    Update UnitTest1.cs

--- ESPL_POS_TEST/UnitTest1.cs
+++ ESPL_POS_TEST/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace ESPL_POS_TEST
         {
             Product p = new Product();
 
-            Product product = p.returnProductList("P345");
+            Product product = p.returnProductList("P124");
 
             Assert.AreEqual(9.90, product.Price);
             
